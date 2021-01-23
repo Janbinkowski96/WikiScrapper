ec7d65a696fc9ad3d8b0c619d9c5acebcabf2b6f
Jan <jan.binkowski96@gmail.com>
2021-01-23 19:51:02 +0100

    Created basic CLI

---
+++ cli.py
@@ -0,0 +1,28 @@
+import click
+import time
+from source.scrapper import Scrapper
+from datetime import timedelta
+
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+    if ctx.invoked_subcommand is None:
+        click.echo("Type 'python cli.py run -p <phrase>' to run scrapper.")
+    else:
+        click.echo(click.style("Running", blink=True))
+
+
+@cli.command()
+@click.option("-p", "--phrase", required=True, type=str)
+def run(phrase: str) -> None:
+    scrapper = Scrapper(phrase)
+    scrapper.get_similar_words()
+    start_time = time.monotonic()
+    scrapper.scrap_raw_data()
+    end_time = time.monotonic()
+    print(timedelta(seconds=end_time - start_time))
+
+
+if __name__ == '__main__':
+    cli()
